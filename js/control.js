6488d8f6b174c303b85f53640b65e5bd8774c5fb
BrayanXactus <brayan.rosales@xactus.io>
2026-02-24 10:51:55 -0500

    fixes

--- js/control.js
+++ js/control.js
@@ -29,15 +29,12 @@ angularAppAQ.controller('principalController', function ($scope, $http, $rootSco
             
             $rootScope.$evalAsync();
     
-            // Solo redirigir si estamos en "/sesion" y ya estamos autenticados
             if ($rootScope.oDatosSesion.bSession && $rootScope.oDatosSesion.sUrlSolicitud === 'sesion') { 
                 $location.url("/");
             } 
-            // No redireccionar en la ruta raíz si ya está autenticado
             else if (!$rootScope.oDatosSesion.bSession && $rootScope.oDatosSesion.sUrlSolicitud !== 'abierta' && $rootScope.oDatosSesion.sUrlSolicitud !== 'sesion' && $rootScope.oDatosSesion.sUrlSolicitud !== 'generalidades' && $rootScope.oDatosSesion.sUrlSolicitud !== '' && $rootScope.oDatosSesion.sUrlSolicitud !== 'cuenca') {
                 $location.url("/");
             }
-            // Verificar permisos para rutas protegidas
             else if ($rootScope.oDatosSesion.bSession && $rootScope.oDatosSesion.sUrlSolicitud.indexOf('edicion') !== -1 && !$rootScope.oDatosSesion.editar_encuesta) {
                 $location.url("/");
             } 
@@ -86,72 +83,12 @@ angularAppAQ.controller('principalController', function ($scope, $http, $rootSco
         $location.url("/" + sMenu);
     }
 
-    $scope.visualizarImagenOld = function (oRegistro, oCampo) {
-
-        //console.log(oCampo);
-
-        /*if (input.files && input.files[0]) {
-            let idVista = 'vpimagen_' + tabla + '_' + campo;
-
-            console.log(idVista);
-
-            let reader = new FileReader();
-            reader.onload = function (e) {
-                let preview = document.getElementById(idVista),
-                    image = document.createElement('img');
-                image.className = "img-fluid";
-                image.style.height = "100%";
-                image.src = reader.result;
-                preview.innerHTML = '';
-                preview.append(image);
-            };
-            reader.readAsDataURL(input.files[0]);
-        }*/
-    };
-
-    /*$scope.menu = function (sMenu) {
-        $scope.sMenuActivo = sMenu;
-    }
-
-    $rootScope.usuario  = function () {
-        $rootScope.form = 'usuario';
-        window.sessionStorage.setItem('form', $rootScope.form);
-        $rootScope.bMostrarEncabezado = true;
-        $rootScope.sShowForm = 'views/usuario.html?i=' + Math.random().toString(36).slice(2);
-
-        console.log($rootScope.sShowForm);
-        console.log($rootScope.bMostrarEncabezado);
-        console.log($rootScope.form);
-    }
-
-    $rootScope.inicio  = function () {
-        $rootScope.form = 'inicio';
-        window.sessionStorage.setItem('form', $rootScope.form);
-        $rootScope.sShowForm = 'views/inicio.html?i=' + Math.random().toString(36).slice(2);
-        $rootScope.bMostrarEncabezado = true;
-
-        console.log($rootScope.sShowForm);
-        console.log($rootScope.bMostrarEncabezado);
-        console.log($rootScope.form);
-    }
-
-    $scope.sMenuActivo = '';
-    $rootScope.sErrorLogin = '';
-    $rootScope.oLogin = { 'correo' : null, 'contrasenia' : null, 'method' : 'iniciarSesion' };
-    $rootScope.oSession = {};
-    $rootScope.form = 'sesion';
-    $rootScope.bMostrarEncabezado = false;
-    $rootScope.sShowForm = '';
-    $rootScope.nombreUsuario = null;
-
-    $rootScope.validarSesion(true);*/
-
     $rootScope.sUrlSolicitud = 'sesion';
     $rootScope.oDatosSesion = { "nombre": null, "sUrlSolicitud": "sesion" };
 
     $rootScope.$on('$routeChangeSuccess', function (event, currentRoute) {
         
-        let sUrlSolicitud = 'sesion'; // valor por defecto
+        let sUrlSolicitud = 'sesion';
         try {
             if (currentRoute && currentRoute.originalPath && typeof currentRoute.originalPath === 'string') {
                 if (currentRoute.originalPath.length > 0 && currentRoute.originalPath !== '/') {
@@ -164,15 +101,6 @@ angularAppAQ.controller('principalController', function ($scope, $http, $rootSco
             console.error('Error procesando currentRoute:', error);
             sUrlSolicitud = 'sesion';
         }
-		//$rootScope.$apply();
-	
-        /*if ($rootScope.sUrlSolicitud !== 'sesion') {
-            $(".header-navbar").show();
-			$("#sticky-wrapper").show();
-        } else {
-            $(".header-navbar").hide();
-			$("#sticky-wrapper").hide();
-        }*/
 		
 		$rootScope.validarSesion(true, sUrlSolicitud);
 		
@@ -194,10 +122,8 @@ angularAppAQ.directive("filesInput", function() {
     return {
         require: "ngModel",
         link: function postLink(scope, elem, attrs, ngModel) {
-            // tipos: basico - dinamico (editar la imagen)
             var tipo = typeof attrs.tipo === 'undefined' ? 'basico': attrs.tipo;
             elem.on("change", function(e) {
-                //var files = elem[0].files;
                 var files = tipo === 'basico' ? elem[0].files: elem[0];
                 ngModel.$setViewValue(files);
             });
